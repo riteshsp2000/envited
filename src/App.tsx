0e9835d7f4589f68d0b9a3840ef544de17b82e99
Ritesh Patil <riteshsp2000@gmail.com>
2022-10-04 22:51:08 +0530

    feat: setup material ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,29 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+
+import { theme } from 'theme';
+
 const LandingPage = lazy(() => import( /* webpackPrefetch: true */ 'pages/Landing'));
 const EventPage = lazy(() => import( /* webpackPrefetch: true */ 'pages/EventPage'));
 const CreateEvent = lazy(() => import( /* webpackPrefetch: true */ 'pages/CreateEvent'));
 
 const App: React.FC = () => {
   return (
-    <Suspense fallback={<p>Loading</p>}>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />}/>
-            <Route path="/create" element={<CreateEvent />} />
-            <Route path="/event" element={<EventPage />}/>
-          </Routes>
-      </BrowserRouter>
-    </Suspense>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Suspense fallback={<p>Loading</p>}>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LandingPage />}/>
+              <Route path="/create" element={<CreateEvent />} />
+              <Route path="/event" element={<EventPage />}/>
+            </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme/index.ts
@@ -0,0 +1,179 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 1000,
+      lg: 1200,
+      xl: 1920
+    }
+  },
+  palette: {
+    grey: {
+      100: '#FBFAFF',
+      200: '#F2F2F2',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#828282',
+      600: '#4F4F4F',
+      700: '#374151',
+      800: '#1F2937',
+      900: '#111827'
+    },
+    action: {
+      active: '#6B7280',
+      focus: 'rgba(55, 65, 81, 0.12)',
+      hover: 'rgba(55, 65, 81, 0.04)',
+      selected: 'rgba(55, 65, 81, 0.08)',
+      disabledBackground: 'rgba(55, 65, 81, 0.12)',
+      disabled: 'rgba(55, 65, 81, 0.26)'
+    },
+    background: {
+      default: '#F9FAFC',
+      paper: '#FFFFFF'
+    },
+    divider: '#E6E8F0',
+    primary: {
+      main: '#501FC1',
+      light: '#8456EC',
+      dark: '#240D57',
+      contrastText: '#FFFFFF'
+    },
+    secondary: {
+      main: '#EDE5FF',
+      light: '#F6F2FF',
+      dark: '#CCB6FF',
+      contrastText: '#000000'
+    },
+    success: {
+      main: '#00805E',
+      light: '#D3FFE2',
+      dark: '#00805E',
+      contrastText: '#FFFFFF'
+    },
+    info: {
+      main: '#2196F3',
+      light: '#64B6F7',
+      dark: '#0B79D0',
+      contrastText: '#FFFFFF'
+    },
+    warning: {
+      main: '#FFB020',
+      light: '#FFBF4C',
+      dark: '#B27B16',
+      contrastText: '#FFFFFF'
+    },
+    error: {
+      main: '#E61445',
+      light: '#DA6868',
+      dark: '#E61445',
+      contrastText: '#FFFFFF'
+    },
+    text: {
+      primary: '#121828',
+      secondary: '#65748B',
+      disabled: 'rgba(55, 65, 81, 0.48)'
+    }
+  },
+  shape: {
+    borderRadius: 8
+  },
+  shadows: [
+    'none',
+    '0px 1px 1px rgba(100, 116, 139, 0.06), 0px 1px 2px rgba(100, 116, 139, 0.1)',
+    '0px 1px 2px rgba(100, 116, 139, 0.12)',
+    '0px 1px 4px rgba(100, 116, 139, 0.12)',
+    '0px 1px 5px rgba(100, 116, 139, 0.12)',
+    '0px 1px 6px rgba(100, 116, 139, 0.12)',
+    '0px 2px 6px rgba(100, 116, 139, 0.12)',
+    '0px 3px 6px rgba(100, 116, 139, 0.12)',
+    '0px 2px 4px rgba(31, 41, 55, 0.06), 0px 4px 6px rgba(100, 116, 139, 0.12)',
+    '0px 5px 12px rgba(100, 116, 139, 0.12)',
+    '0px 5px 14px rgba(100, 116, 139, 0.12)',
+    '0px 5px 15px rgba(100, 116, 139, 0.12)',
+    '0px 6px 15px rgba(100, 116, 139, 0.12)',
+    '0px 7px 15px rgba(100, 116, 139, 0.12)',
+    '0px 8px 15px rgba(100, 116, 139, 0.12)',
+    '0px 9px 15px rgba(100, 116, 139, 0.12)',
+    '0px 10px 15px rgba(100, 116, 139, 0.12)',
+    '0px 12px 22px -8px rgba(100, 116, 139, 0.25)',
+    '0px 13px 22px -8px rgba(100, 116, 139, 0.25)',
+    '0px 14px 24px -8px rgba(100, 116, 139, 0.25)',
+    '0px 10px 10px rgba(31, 41, 55, 0.04), 0px 20px 25px rgba(31, 41, 55, 0.1)',
+    '0px 25px 50px rgba(100, 116, 139, 0.25)',
+    '0px 25px 50px rgba(100, 116, 139, 0.25)',
+    '0px 25px 50px rgba(100, 116, 139, 0.25)',
+    '0px 25px 50px rgba(100, 116, 139, 0.25)'
+  ],
+  typography: {
+    button: {
+      fontWeight: 600
+    },
+    fontFamily:
+      'Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+    body1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.5
+    },
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+      lineHeight: 1.57
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      fontWeight: 500,
+      lineHeight: 1.75
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      lineHeight: 1.57
+    },
+    overline: {
+      fontSize: '0.75rem',
+      fontWeight: 600,
+      letterSpacing: '0.5px',
+      lineHeight: 2.5,
+      textTransform: 'uppercase'
+    },
+    caption: {
+      fontSize: '0.75rem',
+      fontWeight: 400,
+      lineHeight: 1.66
+    },
+    h1: {
+      fontWeight: 700,
+      fontSize: '3.5rem',
+      lineHeight: 1.375
+    },
+    h2: {
+      fontWeight: 700,
+      fontSize: '3rem',
+      lineHeight: 1.375
+    },
+    h3: {
+      fontWeight: 700,
+      fontSize: '2.25rem',
+      lineHeight: 1.375
+    },
+    h4: {
+      fontWeight: 700,
+      fontSize: '2rem',
+      lineHeight: 1.375
+    },
+    h5: {
+      fontWeight: 600,
+      fontSize: '1.5rem',
+      lineHeight: 1.375
+    },
+    h6: {
+      fontWeight: 600,
+      fontSize: '1.125rem',
+      lineHeight: 1.375
+    }
+  }
+});
